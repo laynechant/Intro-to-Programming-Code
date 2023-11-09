87eba0d6a85234e76a45471c7975c97f6239220d
Wudong-champlain <143460294+Wudong-champlain@users.noreply.github.com>
2023-11-09 12:20:42 -0500

    upload code
    here an update to teh code. I talked with the teacher about how to add a heaer and a function files.

---
+++ game.h
@@ -0,0 +1,11 @@
+//
+// Created by Kirit on 11/9/2023.
+//
+
+#ifndef UNTITLED1_GAME_H
+#define UNTITLED1_GAME_H
+void printIntro();
+void fightboss();
+int chooseWeapon();
+
+#endif //UNTITLED1_GAME_H
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "game.h"
+using namespace std;
+
+
+
+int main() {
+    printIntro();
+    fightboss();
+    return 0;
+}
